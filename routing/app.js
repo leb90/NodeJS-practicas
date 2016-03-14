74be00c360c56a4f870950aa5822aa90e729e435
leb90 <leam.90@gmail.com>
2016-03-14 11:04:00 -0300

    para mono

--- routing/app.js
+++ routing/app.js
@@ -3,6 +3,35 @@ var app = express();
 
 var indexController =require("./controller/index");
 
+var user = require('./user');
+var http = require('http');
+var path = require('path');
+ 
+var app = express();
+ 
+
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+ 
+require('./')(app);
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*
 app.set("view engine", "jade");
 
 app.get("/",function(req,res, next){
@@ -10,6 +39,7 @@ app.get("/",function(req,res, next){
 });
 app.get("/:nombre", function(req,res){
 	res.send("form",{nombre: req.params.nombre});
+
 });
 
 app.post("/", function(req,res){
@@ -20,4 +50,5 @@ app.use(function(req, res, next){
 	console.log("tu vieja")
 });
 
-app.listen(8080);
+
+*/
